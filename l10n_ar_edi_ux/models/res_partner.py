8c247be98408997d7b37263cace89e07cedf6857
Felipe Garcia Suez <feg@adhoc.com.ar>
2025-10-29 15:07:12 -0300

    Revert "[ADD] l10n_ar_edi_ux: Adapting the zeep library"
    This reverts commit 5027c49c8d670f114fb1df4ea500e6adf738ab80.

--- l10n_ar_edi_ux/models/res_partner.py
+++ l10n_ar_edi_ux/models/res_partner.py
@@ -1,6 +1,6 @@
 from odoo import models, fields, _
 from odoo.exceptions import UserError
-from odoo.tools.zeep.helpers import serialize_object
+import zeep
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -68,7 +68,7 @@ class ResPartner(models.Model):
         if errors:
             raise UserError(error_msg % (self.name, vat, errors))
 
-        data = serialize_object(res.datosGenerales, dict)
+        data = zeep.helpers.serialize_object(res.datosGenerales, dict)
         if not data:
             raise UserError(error_msg % (self.name, vat, res))
 
@@ -77,8 +77,8 @@ class ResPartner(models.Model):
             raise UserError(error_msg % (self.name, vat, 'La afip no devolvió nombre'))
 
         domicilio = data.get("domicilioFiscal", {})
-        data_mt = serialize_object(res.datosMonotributo, dict) or {}
-        data_rg = serialize_object(res.datosRegimenGeneral, dict) or {}
+        data_mt = zeep.helpers.serialize_object(res.datosMonotributo, dict) or {}
+        data_rg = zeep.helpers.serialize_object(res.datosRegimenGeneral, dict) or {}
         impuestos = [imp["idImpuesto"]
                      for imp in data_mt.get("impuesto", []) + data_rg.get("impuesto", [])
                      if data.get('estadoClave') == 'ACTIVO']
